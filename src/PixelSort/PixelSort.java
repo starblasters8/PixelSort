7c0c5121e42ada26a2284e0407f1c1dbe2b9e727
starblasters8 <starblasters8@gmail.com>
2023-03-28 14:09:45 -0400

    Added HSV conversion

--- src/PixelSort/PixelSort.java
+++ src/PixelSort/PixelSort.java
@@ -8,7 +8,71 @@ import java.util.ArrayList;
 
 public class PixelSort 
 {
-    public void sort(String path) throws Exception
+    public void sortByHSB(String path) throws Exception
+    {
+        File file = new File(path);
+        BufferedImage image = ImageIO.read(file);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        ArrayList<ThreeVal> colOcc = new ArrayList<ThreeVal>();
+        String fileExtension = file.getName().substring(file.getName().lastIndexOf('.') + 1);
+        String pathToExtension = path.substring(0, path.lastIndexOf('.'));
+
+        //Get all colors
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                colOcc.add(new ThreeVal(new Color(image.getRGB(x, y)), 1));
+        
+        //Merge same colors
+        for(int i = 0; i < colOcc.size(); i++)
+        {
+            for(int j = i + 1; j < colOcc.size(); j++)
+            {
+                if(colOcc.get(i).getColor().equals(colOcc.get(j).getColor()))
+                {
+                    colOcc.get(i).setNum(colOcc.get(i).getNum() + colOcc.get(j).getNum());
+                    colOcc.remove(j);
+                    j--;
+                }
+            }
+        }
+
+        //Sort pixels by hsv
+        for(int i = 0; i < colOcc.size(); i++)
+        {
+            for(int j = i + 1; j < colOcc.size(); j++)
+            {
+                if(colOcc.get(i).getHue() > colOcc.get(j).getHue() || (colOcc.get(i).getHue() == colOcc.get(j).getHue() && colOcc.get(i).getSaturation() > colOcc.get(j).getSaturation()) || (colOcc.get(i).getHue() == colOcc.get(j).getHue() && colOcc.get(i).getSaturation() == colOcc.get(j).getSaturation() && colOcc.get(i).getBrightness() > colOcc.get(j).getBrightness()))
+                {
+                    ThreeVal temp = colOcc.get(i);
+                    colOcc.set(i, colOcc.get(j));
+                    colOcc.set(j, temp);
+                }
+            }
+        }
+
+        //Write to file
+        BufferedImage out = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < colOcc.size(); i++)
+        {
+            for(int j = 0; j < colOcc.get(i).getNum(); j++)
+            {
+                out.setRGB(x, y, colOcc.get(i).getColor().getRGB());
+                x++;
+                if(x >= width)
+                {
+                    x = 0;
+                    y++;
+                }
+            }
+        }
+
+        ImageIO.write(out, fileExtension, new File(pathToExtension + "SortedHSV." + fileExtension));
+    }
+
+    public void sortByRGB(String path) throws Exception
     {
         File file = new File(path);
         BufferedImage image = ImageIO.read(file);
@@ -69,12 +133,12 @@ public class PixelSort
             }
         }
 
-        ImageIO.write(out, fileExtension, new File(pathToExtension + "Sorted." + fileExtension));
+        ImageIO.write(out, fileExtension, new File(pathToExtension + "SortedRGB." + fileExtension));
     }
 
     public static void main(String[] args) throws Exception
     {
         PixelSort ps = new PixelSort();
-        ps.sort("lib/Chonk.png");
+        ps.sortByHSB("lib/chonk.png");
     }
 }
--- src/PixelSort/ThreeVal.java
+++ src/PixelSort/ThreeVal.java
@@ -13,6 +13,22 @@ public class ThreeVal
         this.sum = color.getBlue() + color.getGreen() + color.getRed();
     }
 
+    public double getHue()
+    {
+        double hue = (double)(Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null)[0]);
+        return hue;
+    }
+    public double getSaturation()
+    {
+        double saturation = (double)(Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null)[1]);
+        return saturation;
+    }
+    public double getBrightness()
+    {
+        double brightness = (double)(Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null)[2]);
+        return brightness;
+    }
+
     public void setColor(Color color){this.color = color;}
     public void setNum(int num){this.num = num;}
     public Color getColor(){return color;}
